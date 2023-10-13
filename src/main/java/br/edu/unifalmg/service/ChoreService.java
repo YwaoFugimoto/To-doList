31981781bb8e58c842e22e8d2a17c8d472c1f606
Ywao <pedro.fugimoto@sou.unifal-mg.edu.br>
2023-10-13 19:03:57 -0300

    feat: added print function

--- src/main/java/br/edu/unifalmg/service/ChoreService.java
+++ src/main/java/br/edu/unifalmg/service/ChoreService.java
@@ -1,30 +1,35 @@
 package br.edu.unifalmg.service;
 import br.edu.unifalmg.domain.Chore;
-import br.edu.unifalmg.exception.DuplicatedChoreException;
-import br.edu.unifalmg.exception.InvalidDeadLineException;
-import br.edu.unifalmg.exception.InvalidDescriptionException;
-
+import br.edu.unifalmg.exception.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class ChoreService {
 
     private List<Chore> chores;
-    public ChoreService(){
+
+    List<Chore> getChores() {
+        return chores;
+    }
+
+    public ChoreService() {
         chores = new ArrayList<>();
     }
 
-    public Chore addChore(String description, LocalDate deadLine){
-        if ( description == null || description.isEmpty()){
+    public Chore addChore(String description, LocalDate deadLine) {
+        if (description == null || description.isEmpty()) {
             throw new InvalidDescriptionException("The description cannot be null or empty");
         }
-        if(deadLine == null || deadLine.isBefore(LocalDate.now())){
+        if (deadLine == null || deadLine.isBefore(LocalDate.now())) {
             throw new InvalidDeadLineException("The deadline cannot be null or before the current date");
         }
 
-        for (Chore chore : chores){
-            if(chore.getDescription().equals(description) && chore.getDeadLine().equals(deadLine)){
+        for (Chore chore : chores) {
+            if (chore.getDescription().equals(description) && chore.getDeadLine().equals(deadLine)) {
                 throw new DuplicatedChoreException("The given chore already exists.");
             }
         }
@@ -42,5 +47,46 @@ public class ChoreService {
 
         return chore;
     }
+
+    /**
+     * Method to delete a given chore
+     *
+     * @param description The description of the chore
+     * @param deadline    The deadline of the chora
+     */
+    public void deleteChore(String description, LocalDate deadline) {
+        if (this.chores.isEmpty()) {
+            throw new EmptyChoreListException("Unable to remove a chore from an empty list");
+        }
+        boolean isChoreExist = this.chores.stream().anyMatch((chore -> chore.getDescription().equals(description)
+                && chore.getDeadLine().isEqual(deadline)));
+        if (!isChoreExist) {
+            throw new ChoreNotFoundException("The given chore does not exist.");
+        }
+        this.chores = this.chores.stream().filter(chore -> !chore.getDescription().equals(description)
+                && !chore.getDeadLine().isEqual(deadline)).collect(Collectors.toList());
+    }
+    //private Predicate<List<Chore>> isChoreListEmpty = List::isEmpty;
+
+    public void toggleChore(String description, LocalDate deadLine) {
+
+    }
+
+    public String printChores() {
+        if (isChoreListEmpty.test(this.chores)) {
+            throw new EmptyChoreListException("Unable to print a chore from an empty list");
+        }
+        StringBuilder print = new StringBuilder();
+        for (Chore chore : chores) {
+            if (chore.getIsCompleted()) {
+                print.append("Description: ").append(chore.getDescription()).append(" / Deadline: ").append(chore.getDeadline()).append(" / Status: Completed\n");
+            } else {
+                print.append("Description: ").append(chore.getDescription()).append(" / Deadline: ").append(chore.getDeadline()).append(" / Status: Incompleted\n");
+            }
+        }
+        return print.toString();
+    }
+
+    private final Predicate<List<Chore>> isChoreListEmpty = choreList -> choreList.isEmpty();
 
 }
--- src/test/java/br/edu/unifalmg/service/choreServiceTest.java
+++ src/test/java/br/edu/unifalmg/service/choreServiceTest.java
@@ -1,19 +1,20 @@
 package br.edu.unifalmg.service;
 
-import br.edu.unifalmg.exception.DuplicatedChoreException;
-import br.edu.unifalmg.exception.InvalidDeadLineException;
-import br.edu.unifalmg.exception.InvalidDescriptionException;
+import br.edu.unifalmg.domain.Chore;
+import br.edu.unifalmg.exception.*;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.ReflectionUtils;
 
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class choreServiceTest {
 
     @Test
+    @DisplayName("#addChore")
     void addChoreWhenTheDescriptionIsInvalidThrowAnException() {
         ChoreService service = new ChoreService();
         assertAll(
@@ -56,4 +57,63 @@ public class choreServiceTest {
                 ()-> service.addChore("Description", LocalDate.now()));
     }
 
+    @Test
+    @DisplayName("#deleteChore > When the list is empty > ")
+    void deleteChoreWhenTheListIsEmptyThrowAnException(){
+        ChoreService service = new ChoreService();
+        assertThrows(EmptyChoreListException.class, () -> {
+            service.deleteChore("Anything", LocalDate.now());
+        });
+    }
+
+    @Test
+    @DisplayName("#deleteChore > When the list in not empty > When the chore does not exists")
+    void deleteChoreWhenTheListIsNotEmptyWhenTheChoreDoesNotExistThrowAnException() {
+        ChoreService service = new ChoreService();
+        service.addChore("Description", LocalDate.now());
+        assertThrows(ChoreNotFoundException.class, () -> {
+            service.deleteChore("Chore to be deleted", LocalDate.now().plusDays(1));
+        });
+    }
+
+    @Test
+    @DisplayName("#deleteChore > When the list is not empty > When the chore exists > Delete the chore")
+    void deleteChoreWhenTheListIsNotEmptyWhenTheChoreExistsDeleteTheChore(){
+        ChoreService service = new ChoreService();
+        service.addChore("Chore #01", LocalDate.now().plusDays(1));
+        assertEquals(1, service.getChores().size());
+        service.deleteChore("Chore #01", LocalDate.now().plusDays(1));
+        assertEquals(0, service.getChores().size());
+    }
+
+    @Test
+    @DisplayName("#toggleChore > When the deadline is valid > Toggle the Chore")
+    void toggleChoreWhenTheDeadlineIsValidToggleTheChore(){
+        ChoreService service = new ChoreService();
+        service.addChore("Chore #01", LocalDate.now());
+        Assertions.assertFalse(service.getChores().get(0).getIsCompleted());
+        assertDoesNotThrow(()-> service.toggleChore("Chore #01", LocalDate.now()));
+
+        Assertions.assertTrue(service.getChores().get(0).getIsCompleted());
+    }
+
+    @Test
+    @DisplayName("#printChores > When the list is empty > throw exception")
+    void printChoresWhenTheListIsEmptyThrowAnException(){
+        ChoreService service = new ChoreService();
+        assertThrows(EmptyChoreListException.class, ()-> service.printChores());
+    }
+
+    @Test
+    @DisplayName("#printChores > When the list is not empty > Print the descriptions")
+    void printChoresWhenTheListIsNotEmptyPrintTheDescriptions(){
+        ChoreService service = new ChoreService();
+        service.addChore("Chore 1",LocalDate.now().plusDays(5));
+        service.addChore("Chore 2",LocalDate.now().plusDays(3));
+        service.addChore("Chore 3",LocalDate.now().plusDays(2));
+        assertEquals("Description: Chore 1 / Deadline: 2023-10-18 / Status: Incompleted\n" +
+                        "Description: Chore 2 / Deadline: 2023-10-16 / Status: Incompleted\n" +
+                        "Description: Chore 3 / Deadline: 2023-10-15 / Status: Incompleted\n",
+                service.printChores());
+    }
 }
